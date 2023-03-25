cd8411d6d97a440961b4f25a714a19f58b0a4438
sahazu <82225736+sahazu@users.noreply.github.com>
2023-03-25 17:45:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher, executor, types
-from dotenv import load_dotenv
 import openai
 
-TOKEN = 'YOUR TELEGRAM BOT KEY'
-openai.api_key = "YOUR OPEN AI API KEY"
+TOKEN = 'YOUR_BOT_TOKEN'
+openai.api_key = "YOUR_OPENAI_KEY"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
@@ -17,17 +16,45 @@ messages=[
     {"role": "user", "content": "Where was it played?"}
 ]
 
+promptHis = []
+
 def update(messages, role, content):
     messages.append({"role": role, "content": content})
     return messages
 
+
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
+    global access
+    global messages
     await message.answer("Привет, в этом боте ты можешь использовать <b>GPT 3.5</b>, просто отправь свой запрос", parse_mode=types.ParseMode.HTML)
+
+@dp.message_handler(commands=['clear'])
+async def start(message: types.Message):
+    global messages
+    messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    {"role": "user", "content": "Where was it played?"}
+]
+    await message.answer("История очищена", parse_mode=types.ParseMode.HTML)
+
+@dp.message_handler(commands=['re'])
+async def start(message: types.Message):
+    sent_message = await message.answer("<b>ChatGPT регенерирует ответ...</b>", parse_mode=types.ParseMode.HTML)
+    update(messages, "user", promptHis[len(promptHis) - 1])
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages,
+    )
+    await bot.delete_message(chat_id=sent_message.chat.id, message_id=sent_message.message_id)
+    await message.answer(response['choices'][0]['message']['content'])
 
 @dp.message_handler()
 async def prompt(message: types.Message):
-    if message.text != "start":
+    if message.text != "/start" or "/clear" or "/re":
+        promptHis.append(message.text)
         sent_message = await message.answer("<b>ChatGPT генерирует ответ...</b>", parse_mode=types.ParseMode.HTML)
         update(messages, "user", message.text)
         response = openai.ChatCompletion.create(
@@ -36,6 +63,8 @@ async def prompt(message: types.Message):
         )
         await bot.delete_message(chat_id=sent_message.chat.id, message_id=sent_message.message_id)
         await message.answer(response['choices'][0]['message']['content'])
+
+
 
 async def main():
     await dp.start_polling(bot)
